9aa58f8603142dd9e21fa9f3a0c10a4958f8ee80
Andy Menutti <andymenutti@gmail.com>
2022-06-23 12:35:32 -0300

    add val.js

--- val.js
+++ val.js
@@ -1,8 +1,6 @@
 const form = document.getElementById('form');
-const username = document.getElementById('username');
+const username = document.getElementById('name');
 const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -36,8 +34,6 @@ const isValidEmail = email => {
 const validateInputs = () => {
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
 
     if(usernameValue === '') {
         setError(username, 'Username is required');
@@ -52,21 +48,4 @@ const validateInputs = () => {
     } else {
         setSuccess(email);
     }
-
-    if(passwordValue === '') {
-        setError(password, 'Password is required');
-    } else if (passwordValue.length < 8 ) {
-        setError(password, 'Password must be at least 8 character.')
-    } else {
-        setSuccess(password);
-    }
-
-    if(password2Value === '') {
-        setError(password2, 'Please confirm your password');
-    } else if (password2Value !== passwordValue) {
-        setError(password2, "Passwords doesn't match");
-    } else {
-        setSuccess(password2);
-    }
-
 };
